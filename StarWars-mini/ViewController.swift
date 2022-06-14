c2ebf8752a5ca7c906deff355fc258a4d158b1c9
Hovhannes Mikayelyan <hovmikayelyan@gmail.com>
2022-06-14 18:17:49 +0400

    Initial Commit

---
+++ StarWars-mini/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StarWars-mini
+//
+//  Created by Hovhannes Mikayelyan on 6/14/22.
+//  Copyright © 2022 Hovhannes Mikayelyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
