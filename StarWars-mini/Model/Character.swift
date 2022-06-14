5bd732409fdd47fac2894043b3f2b954465f16d0
Hovhannes Mikayelyan <hovmikayelyan@gmail.com>
2022-06-14 18:50:52 +0400

    Initial commit

---
+++ StarWars-mini/Model/Character.swift
@@ -0,0 +1,9 @@
+//
+//  Character.swift
+//  StarWars-mini
+//
+//  Created by Hovhannes Mikayelyan on 6/14/22.
+//  Copyright © 2022 Hovhannes Mikayelyan. All rights reserved.
+//
+
+import Foundation
---
+++ StarWars-mini/Model/Planet.swift
@@ -0,0 +1,9 @@
+//
+//  Planet.swift
+//  StarWars-mini
+//
+//  Created by Hovhannes Mikayelyan on 6/14/22.
+//  Copyright © 2022 Hovhannes Mikayelyan. All rights reserved.
+//
+
+import Foundation
